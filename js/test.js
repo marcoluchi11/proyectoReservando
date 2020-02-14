241ead5520b09528256de8cf2c0b07772e9fec39
Marco Luchi <marcoaluchi@hotmail.com>
2020-02-14 16:10:47 -0300

    se termina reservarHorario y obtenerPuntuacion

--- js/test.js
+++ js/test.js
@@ -1,6 +1,6 @@
 var expect = chai.expect;
 
-describe('Reserva de Horario',function(){
+describe('funcion reservarHorario()',function(){
 
         it('Busca horario dentro del arreglo de horarios y lo elimina',function(){
 
@@ -17,5 +17,46 @@ describe('Reserva de Horario',function(){
             expect(listadoDeRestaurantes[2].horarios.length).to.equal(cantidadPrevia);
         })
 
+        it('Se reserva un horario pero no se le pasa ningún parámetro a la función, el arreglo se mantiene igual.',function(){
+
+
+            var cantidadPrevia = listadoDeRestaurantes[2].horarios.length;
+            listadoDeRestaurantes[2].reservarHorario('');
+            expect(listadoDeRestaurantes[2].horarios.length).to.equal(cantidadPrevia);   
+        })
+})
+
+describe('funcion Obtener Puntuacion()', function(){
+
+    it('Dado un restaurant con determinadas calificaciones, la puntuación (que es el promedio de ellas) se calcula correctamente.',function(){
+
+                var notas = listadoDeRestaurantes[1].calificaciones;
+                var suma = 0;
+                notas.forEach(function(elem){
+                            suma += elem;
+                })
+                var promedio =  suma / notas.length;
+                suma = Math.round(promedio * 10) / 10;
+                expect(suma).to.equal(listadoDeRestaurantes[1].obtenerPuntuacion());
+    })
+    it('Dado un restaurant que no tiene ninguna calificación, la puntuación es igual a 0.',function(){
+
+            if(listadoDeRestaurantes[2].calificaciones.length === 0){
+
+
+                    expect(listadoDeRestaurantes[2].obtenerPuntuacion()).to.equal(0);
+            }
+
+    })
+})
+
+describe('funcion calificar()',function(){
+
+
+        it('',function(){
+
+
+            
+        })
 
 })
